460b633e192e19222e56293999201e7884d6d677
mohamed korny <mohamed korny>
2017-02-22 13:20:59 +0100

    tp3

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 var express = require( 'express' );
 var request = require( 'request' );
 var cheerio = require( 'cheerio' );
-url = 'https://www.leboncoin.fr/ventes_immobilieres/1076257949.htm?ca=12_s';
 
 //creating a new express server
 var app = express();
@@ -14,40 +13,104 @@ app.set( 'view engine', 'ejs' );
 app.use( '/assets', express.static( 'assets' ) );
 
 
-//makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
-
-/*
 app.get( '/', function ( req, res ) {
-    request( 'https://www.leboncoin.fr/ventes_immobilieres/1076257949.htm?ca=12_s [https://www.leboncoin.fr/ventes_immobilieres/1076257949.htm?ca=12_s_green]', function ( error, response, body ) {
-        if ( !error && response.statusCode == 200 ) {
 
-            var $ = cheerio.load( body )
+    res.render( 'home', {
+        message_1: '',
+        message_2: '',
+        message_3: '',
+        message_4: '',
 
-            console.log( $( 'div.line h2.item_price.clearfix span.value ' ).get() );
-
-            res.render( 'home', {
-                message: ''
-
-            });
-        }
-    })
-});
-*/
-
-app.get( '/', function ( req, res ) {
-    request( url, function ( error, response, body ) {
-        if ( !error ) {
-            var $ = cheerio.load( body ),
-                data = $( ".selector" ).html();
-            console.log( data );
-        } else {
-            console.log( "Erreur : " + error );
-        }
     });
-
 });
-//launch the server on the 3000 port
+
 app.listen( 3000, function () {
     console.log( 'App listening on port 3000!' );
 });
+
+app.get( '/compare', function ( req, res ) {
+    //var url = req.query.urlLBC
+    var url = req.query.urlLbc
+
+    request( url, function ( error, response, html ) {
+        if ( !error && response.statusCode ) {
+
+            var $ = cheerio.load( html )
+
+            var lbcDataArray = $( 'section.properties span.value' )
+
+            var lbcData = {
+                price: parseInt( $( lbcDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
+                city: $( lbcDataArray.get( 1 ) ).text().trim().toLowerCase().replace( /\_|\s/g, '-' ),
+                type: $( lbcDataArray.get( 2 ) ).text().trim().toLowerCase(),
+                surface: parseInt( $( lbcDataArray.get( 4 ) ).text().replace( /\s/g, '' ), 10 )
+            }
+            console.log( 'data:', lbcData )
+        }
+        else {
+            console.log( 'error:', error )
+        }
+
+        var url2 = 'https://www.meilleursagents.com/prix-immobilier/' + lbcData.city.toLowerCase()
+
+        request( url2, function ( error, response, html ) {
+            if ( !error && response.statusCode ) {
+
+                var $ = cheerio.load( html )
+
+                var estData = {
+                    pric_met_maison: parseInt( $( $( $( '.prices-summary__values .row' )[2] ).find( '.columns' )[2] ).text().replace( /\s/g, '' ), 10 ),
+                    pric_met_appart: parseInt( $( $( $( '.prices-summary__values .row' )[1] ).find( '.columns' )[2] ).text().replace( /\s/g, '' ), 10 )
+                }
+                console.log( 'data:', estData )
+            }
+            else {
+                console.log( 'error:', error )
+            }
+
+            if ( lbcData.type == 'maison' ) {
+                var calcul_prix = estData.pric_met_maison * lbcData.surface
+                if ( calcul_prix > lbcData.price ) {
+                    res.render( 'home', {
+                        message_1: 'Type de l\'immobilier: ' + lbcData.type,
+                        message_2: 'Ville: ' + lbcData.city,
+                        message_3: 'Prix: ' + lbcData.price + ' €',
+                        message_4: 'Le prix du bien est en dessous de la moyenne pour cette ville'
+                    });
+                }
+                else {
+                    res.render( 'home', {
+                        message_1: 'Type de l\'immobilier: ' + lbcData.type,
+                        message_2: 'Ville: ' + lbcData.city,
+                        message_3: 'Prix: ' + lbcData.price + ' €',
+                        message_4: 'Le prix du bien est au dessus de la moyenne pour cette ville'
+                    });
+                }
+            }
+
+            if ( lbcData.type == 'appartement' ) {
+                var calcul_prix = estData.pric_met_appart * lbcData.surface
+                if ( calcul_prix > lbcData.price ) {
+                    res.render( 'home', {
+                        message_1: 'Type de l\'immobilier: ' + lbcData.type,
+                        message_2: 'Ville: ' + lbcData.city,
+                        message_3: 'Prix: ' + lbcData.price + ' €',
+                        message_4: 'Le prix du bien est en dessous de la moyenne pour cette ville'
+                    });
+                }
+                else {
+                    res.render( 'home', {
+                        message_1: 'Type de l\'immobilier: ' + lbcData.type,
+                        message_2: 'Ville: ' + lbcData.city,
+                        message_3: 'Prix: ' + lbcData.price + ' €',
+                        message_4: 'Le prix du bien est au dessus de la moyenne pour cette ville'
+                    });
+                }
+            }
+
+        })
+
+    })
+});
+
 
